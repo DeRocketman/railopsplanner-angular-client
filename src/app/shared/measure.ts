d47bc89bd0d78e6752cc700ade204c483a48279b
stricker <de.rocketman@outlook.com>
2022-04-25 22:44:02 +0200

    add measure form and style

--- src/app/shared/measure.ts
+++ src/app/shared/measure.ts
@@ -1,30 +1,53 @@
 export interface Measure {
   id: number;
   name: string;
-  begin: Date;
+  start: Date;
   end: Date;
+  station: string;
   railNetwork: string;
   reasons: MeasureReason[];
   effects?: string;
   passengerConcept?: string;
   lossKilometer?: number;
-  cancellationList?: Cancellation[];
-  timetableChanges?: TimetableChange[];
   kigbauNumber?:string;
+  trainFailures?: TrainFailure[];
+  scheduleDeviations?: ScheduleDeviation[];
 }
 
 export interface ToDoListItem {
+  measureRef: number;
 
 }
 
-export interface Cancellation {
 
-}
-
-export interface TimetableChange {
-
-}
 
 export interface MeasureReason {
+  reason: string;
+  vzgList: string[];
+  startOperationControlPoint: string;
+  endOperationControlPoint: string;
+  operatingMode: string;
+  start: Date;
+  end: Date;
+  disturbed: boolean;
+}
 
+export interface TrainFailure {
+  trafficDay: Date;
+  trainNumber: string;
+  trainType: string;
+  startOperationControlPoint: string;
+  destinationOperationControlPoint: string;
+  failureFrom: string;
+  failureTo: string;
+}
+
+export interface ScheduleDeviation {
+  trafficDay: Date;
+  trainNumber: string;
+  trainType: string;
+  startOperationControlPoint: string;
+  destinationOperationControlPoint: string;
+  deviationType: string;
+  time: number;
 }
